d1c2c843756bf42188f95c144b93bfe015419910
sachin kumar <dev.sachin207@gmail.com>
2025-02-18 13:15:21 +0530

    prisma db setuped with models

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-import { DM_Sans} from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Plura",
+  title: "CMS",
   description: "All in one Agency Solution",
 };
 
@@ -18,16 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
-      <html lang="en">
-        <body className={font.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
               <SignUpButton />
@@ -36,10 +34,9 @@ export default function RootLayout({
               <UserButton />
             </SignedIn>
           </header> */}
-            {children}
-          </ThemeProvider>{" "}
-        </body>
-      </html>
-    </ClerkProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,13 +1,51 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/site", "/api/uploadthing"]);
 
-export default clerkMiddleware(async (authPromise, request) => {
-  const auth = await authPromise; // Await the promise to get the auth object
+export default clerkMiddleware(async (authPromise, req) => {
+  const auth = await authPromise;
 
-  if (!isPublicRoute(request)) {
-    auth.protect(); // Enforce authentication
+  if (!isPublicRoute(req)) {
+    auth.protect();
+  }
+
+  const url = req.nextUrl;
+  const searchParams = url.searchParams.toString();
+  const hostname = req.headers;
+
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+
+  //if subdomain exists
+  const customSubDomain = hostname
+    .get("host")
+    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    .filter(Boolean)[0];
+
+  if (customSubDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    );
+  }
+
+  if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+    return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
+  }
+
+  if (
+    url.pathname === "/" ||
+    (url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL("/site", req.url));
+  }
+
+  if (
+    url.pathname.startsWith("/agency") ||
+    url.pathname.startsWith("/subaccount")
+  ) {
+    return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url));
   }
 });
 // export default clerkMiddleware();
